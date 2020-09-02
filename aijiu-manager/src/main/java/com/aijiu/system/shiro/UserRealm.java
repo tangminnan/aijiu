4a8156fca2c6c9ee384b89b79a3b46a29a210970
zouying <349829327@qq.com>
2020-09-02 15:36:53 +0800

    ~

--- aijiu-manager/src/main/java/com/aijiu/system/shiro/UserRealm.java
+++ aijiu-manager/src/main/java/com/aijiu/system/shiro/UserRealm.java
@@ -26,7 +26,7 @@ import com.aijiu.system.service.MenuService;
 
 public class UserRealm extends AuthorizingRealm {
 /*	@Autowired
-	UserDao userMapper;
+	UserDODao userMapper;
 	@Autowired
 	MenuService menuService;*/
 
