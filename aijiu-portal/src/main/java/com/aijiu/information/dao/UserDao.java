27aa771ec490312bacd6acc7a763b899c3e8b827
zouying <349829327@qq.com>
2021-03-11 10:25:26 +0800

    ~

---
+++ aijiu-portal/src/main/java/com/aijiu/information/dao/UserDao.java
@@ -0,0 +1,37 @@
+package com.aijiu.information.dao;
+
+import com.aijiu.information.domain.UserDO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用户信息表
+ * @author wjl
+ * @email bushuo@163.com
+ * @date 2020-08-17 14:43:46
+ */
+@Mapper
+public interface UserDao {
+
+	UserDO get(Long id);
+	
+	List<UserDO> list(Map<String, Object> map);
+	
+	int count(Map<String, Object> map);
+	
+	int save(UserDO user);
+	
+	int update(UserDO user);
+	
+	int remove(Long id);
+	
+	int batchRemove(Long[] ids);
+
+    List<UserDO> listByShouCangLeaveId(Integer leaveId);
+
+	UserDO getUserDO(String openId);
+
+    UserDO getUserDOByOpenId(String openId);
+}
